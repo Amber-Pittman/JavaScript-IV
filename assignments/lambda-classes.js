cbae1fe53c5eb25fc1e480be58c4bcbf7c6c5b2f
Amber Pittman <amber@amberpittman.com>
2019-04-25 08:17:15 -0400

    Created Student Class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -17,38 +17,6 @@ const fred = new Instructor({
 });
 ```
 
-#### Person
-
-* First we need a Person class. This will be our `base-class`
-* Person receives `name` `age` `location` `gender` all as props
-* Person receives `speak` as a method.
-* This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
-
-#### Instructor
-
-* Now that we have a Person as our base class, we'll build our Instructor class.
-* Instructor uses the same attributes that have been set up by Person
-* Instructor has the following unique props:
-  * `specialty` what the Instructor is good at i.e. 'redux'
-  * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
-  * `catchPhrase` i.e. `Don't forget the homies`
-* Instructor has the following methods:
-  * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
-  * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
-
-#### Student
-
-* Now we need some students!
-* Student uses the same attributes that have been set up by Person
-* Student has the following unique props:
-  * `previousBackground` i.e. what the Student used to do before Lambda School
-  * `className` i.e. CS132
-  * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
-* Student has the following methods:
-  * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
-  * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
-  * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
-
 #### Project Manager
 
 * Now that we have instructors and students, we'd be nowhere without our PM's
@@ -106,3 +74,32 @@ class Instructor extends Person {
     }     // Should I change name to this.name?
 }
 
+// #### Student
+
+// * Now we need some students!
+// * Student uses the same attributes that have been set up by Person
+// * Student has the following unique props:
+//   * `previousBackground` i.e. what the Student used to do before Lambda School
+//   * `className` i.e. CS132
+//   * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
+// * Student has the following methods:
+//   * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
+//   * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
+//   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
+class Student extends Person {
+    constructor(studentAttrs) {
+        super(studentAttrs);
+        this.previousBackground = studentAttrs.previousBackground;
+        this.className = studentAttrs.className;
+        this.favSubjects = studentAttrs.favSubjects;
+    }
+    listsSubjects() {
+        console.log(this.favSubjects); // double check that this does an array. I think this is wrong
+    }
+    PRAssignment() {
+        console.log(`${this.name} has submitted a PR for ${this.specialty}.`);
+    }
+    sprintChallenge() {
+        console.log(`${this.name} has begun sprint challenge on ${this.specialty}.`);
+    }
+}
