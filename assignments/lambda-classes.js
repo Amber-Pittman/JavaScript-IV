c35c7ff54d038939da1be4aa9ced753e94eae8dd
Amber Pittman <amber@amberpittman.com>
2019-04-24 21:51:46 -0400

    Setting up my conversion code from refactor file

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,32 @@
 // CODE here for your Lambda Classes
+function GameObject(attributes) {
+    this.createdAt = attributes.createdAt;
+    this.name = attributes.name;
+    this.dimensions = attributes.dimensions;
+  }
+  
+    GameObject.prototype.destroy = function () {
+      return `${this.name} was removed from the game.`;
+    }
+
+function CharacterStats(childAttrs) {
+    GameObject.call(this, childAttrs);
+    this.healthPoints = childAttrs.healthPoints;
+    }
+    CharacterStats.prototype = Object.create(GameObject.prototype);
+
+    CharacterStats.prototype.takeDamage = function () {
+    return `${this.name} took damage.`;
+  }
+
+function Humanoid(grandchildAttrs) {
+    CharacterStats.call(this, grandchildAttrs);
+    this.team = grandchildAttrs.team;
+    this.weapons = grandchildAttrs.weapons;
+    this.language = grandchildAttrs.language;
+    }
+    Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+    Humanoid.prototype.greet = function() {
+        return `${this.name} offers a greeting in ${this.language}.`;
+  } 
