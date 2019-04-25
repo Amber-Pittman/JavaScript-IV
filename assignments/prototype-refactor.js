99e6b9770f3496b0b98190afafdd4019ed01d5ff
Amber Pittman <amber@amberpittman.com>
2019-04-25 08:00:07 -0400

    Created Person Class

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -92,4 +92,4 @@ console.log(mage.weapons); // Staff of Shamalama
 console.log(archer.language); // Elvish
 console.log(archer.greet()); // Lilith offers a greeting in Elvish.
 console.log(mage.takeDamage()); // Bruce took damage.
-console.log(swordsman.destroy());
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
